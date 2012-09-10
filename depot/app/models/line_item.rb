2d068601105591da55d9184d20cff2a6c30155b7
Jun Sitow <mottipok@hotmail.com>
2012-09-10 16:31:11 +0900

    もっとスマートなカート　~p122
    dbサイドで実装すべき内容とアプリケーションサイドで実装すべき内容の切り分け方が不明

--- depot/app/models/line_item.rb
+++ depot/app/models/line_item.rb
@@ -2,4 +2,7 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :product, :quantity, :cart_id, :id
   belongs_to :product
   belongs_to :cart
+  def total_price
+    product.price * quantity
+  end
 end
