fa2b3ee89c3e718cf4e9ed77d0cb0f7397ffbe26
Jun Sitow <mottipok@hotmail.com>
2012-09-12 17:33:50 +0900

    チェックアウト　~p165
    注文情報の取得
    Atomフィード
    ページネーション

--- depot/app/models/line_item.rb
+++ depot/app/models/line_item.rb
@@ -2,6 +2,7 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :product, :quantity, :cart_id, :id, :price
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
   def total_price
     product.price * quantity
   end
